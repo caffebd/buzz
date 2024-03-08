158daaff515a22913da244420ecb593e2705a810
CAFFEBD <caffebd@gmail.com>
2024-03-08 22:50:44 +0000

    Luke Update Mar 8b

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,14 +1,17 @@
 extends Node3D
 
+var barrier_follow: bool = false
+@export var player: CharacterBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	%Barrier.global_position.y = 20.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if barrier_follow:
+		%Barrier.global_position.z = player.global_position.z + 1.0
 
 
 func _on_end_body_entered(body: Node3D) -> void:
@@ -17,3 +20,10 @@ func _on_end_body_entered(body: Node3D) -> void:
 		
 func _load_main():
 	get_tree().change_scene_to_file("res://theCave.tscn")
+
+
+func _on_barrier_trigger_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		%Barrier.global_position.y = 1.0
+		#barrier_follow = true
+	
